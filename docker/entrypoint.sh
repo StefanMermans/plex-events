83db9add07f33956a584a99ab8e1ca545dad44e5
Stefan Mermans <stefanmermans99@gmail.com>
2026-01-11 16:57:35 +0100

    Nginx loggin

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,9 @@
 # Substitute environment variables in nginx config
 envsubst '${FRONTEND_DOMAIN} ${API_DOMAIN}' < /etc/nginx/http.d/default.conf.template > /etc/nginx/http.d/default.conf
 
+# Test nginx config and show any errors
+echo "Testing nginx configuration..."
+nginx -t
+
 # Start supervisord
 exec /usr/bin/supervisord -c /etc/supervisord.conf
